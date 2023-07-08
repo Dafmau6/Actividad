1624cdd16361771cda9ea6595ce5f583ee6f899c
E301 sede B Tecnologico Comfenalco <E301@Academico.local>
2023-07-07 19:23:14 -0500

    commit second

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const connect = require('./config/db');
 const profesoresRoutes = require('./routes/profesores');
+const cors= require('cors')
+
 const app = express();
 connect();
+
 app.use(express.json());
 app.use('/profesores', profesoresRoutes);
-
+app.use(cors());
 const port = process.env.PORT || 3000;
 app.listen(port,
     () => console.log(`La aplicación está
